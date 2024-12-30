193cebfa5351e313e20d9900df08d83759d8cb74
Gian Avila <avila.gian111@gmail.com>
2024-12-30 21:09:51 +0700

    fix: update environment variable for application port

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,6 +1,6 @@
 const ENV = {
   NODE_ENV: process.env.NODE_ENV || 'development',
-  APP_PORT: Number(process.env.PORT) || 8080,
+  APP_PORT: Number(process.env.APP_PORT) || 8080,
   DATABASE_URL: process.env.DATABASE_URL || 'mysql://root:clip123@localhost:3306/clip',
   APP_API_URL: process.env.APP_API_URL || 'http://localhost:8080',
   APP_JWT_SECRET: process.env.APP_JWT_SECRET,
