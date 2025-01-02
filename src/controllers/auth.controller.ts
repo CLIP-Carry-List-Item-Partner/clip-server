8509fee1692f4ba62d51e1d9576e51e6501b931f
Gian Avila <avila.gian111@gmail.com>
2025-01-02 21:22:35 +0700

    feat: add Google OAuth2 client ID and secret to environment configuration

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -25,6 +25,7 @@ const oauth2Client = new google.auth.OAuth2(
   process.env.GOOGLE_CLIENT_ID,
   process.env.GOOGLE_CLIENT_SECRET,
   'https://clip-hub.tech/auth/google/callback'
+  // awalnya http://localhost:8080/auth/google/callback
 )
 
 const userScopes = [
--- src/utils/env.ts
+++ src/utils/env.ts
@@ -4,9 +4,10 @@ const ENV = {
   DATABASE_URL: process.env.DATABASE_URL || 'mysql://root:clip123@localhost:3306/clip',
   APP_API_URL: process.env.APP_API_URL || 'http://localhost:8080',
   APP_JWT_SECRET: process.env.APP_JWT_SECRET,
-  APP_JWT_REFRESH_SECRET:
-    process.env.APP_JWT_REFRESH_SECRET,
+  APP_JWT_REFRESH_SECRET: process.env.APP_JWT_REFRESH_SECRET,
   APP_FRONTEND_URL: process.env.APP_FRONTEND_URL || 'https://localhost:5173/',
-}
+  GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID || 'pakdareensahabatpaksamuel',
+  GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET || 'rafaelsahabatdylan'
+};
 
 export default ENV;
